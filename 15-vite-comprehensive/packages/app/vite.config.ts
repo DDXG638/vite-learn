d1864c23d4299df9afaefcac9b2f77cc36faabd8
Dori <981926536@qq.com>
2026-04-07 01:55:38 +0800

    fix: 增加 vite-bundle-analyzer 对打包结果进行分析; 修改manualChunkd把 node_modules 的依赖打到 vendor chunk 中

--- 15-vite-comprehensive/packages/app/vite.config.ts
+++ 15-vite-comprehensive/packages/app/vite.config.ts
@@ -1,30 +1,51 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import analyzer from 'vite-bundle-analyzer'
 
-export default defineConfig({
-  base: '/cool',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@components': resolve(__dirname, '../components/src'),
-      '@utils': resolve(__dirname, '../utils/src')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {}
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
+export default defineConfig(({ mode }) => {
+  return {
+    base: '/cool',
+    plugins: [
+      vue(),
+      mode === 'analyze' && analyzer({ analyzerMode: 'static' }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@components': resolve(__dirname, '../components/src'),
+        '@utils': resolve(__dirname, '../utils/src')
       }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {}
+      }
+    },
+    define: {
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html')
+        },
+        output: {
+          manualChunks(id) {
+            // 把依赖都打到vendor chunk中
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+
+            return null
+          },
+        }
+      },
+    },
+    ssr: {
+      noExternal: ['@vite-comprehensive/components', '@vite-comprehensive/utils']
     }
-  },
-  ssr: {
-    noExternal: ['@vite-comprehensive/components', '@vite-comprehensive/utils']
   }
 })
